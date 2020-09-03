8fb3a0f5851bb32570765b88f10b3dff1a4f8a01
Your Name <you@example.com>
2020-09-03 14:19:33 +0800

    yxdtest

---
+++ venv/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*
+
+from Interface_test_automation.case.Monitortest import MonitorTestCase
+
+if __name__ == '__main__':
+	“测试一下OK”
+    app = MonitorTestCase()
+    app.runAllCase("新监控后台")
